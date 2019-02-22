c5cbebe164193c09b93359871c84d955dcf4c06b
Syed Hussain Ather <shussainather@gmail.com>
2019-02-21 19:55:05 -0500

    initial commit

---
+++ justificationmap.py
@@ -0,0 +1,7 @@
+"""
+Script that can analyze text for soundness and completeness in arguments.
+Require user to justify claims using other claims in the text.
+Create a logical map that shows how claims are justified.
+Call this a justification map.
+"""
+
