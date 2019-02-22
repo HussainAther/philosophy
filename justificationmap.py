24647d8b45cd87147d0f2f689fd4fbfaf65cb731
Syed Hussain Ather <shussainather@gmail.com>
2019-02-21 19:58:09 -0500

    argv

--- justificationmap.py
+++ justificationmap.py
@@ -1,7 +1,11 @@
+import sys
+
 """
 Script that can analyze text for soundness and completeness in arguments.
 Require user to justify claims using other claims in the text.
 Create a logical map that shows how claims are justified.
 Call this a justification map.
 """
+file = sys.argv[1]
 
+with open()
