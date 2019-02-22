6e3b81fab71bab932897ddc88308687332631ad1
Syed Hussain Ather <shussainather@gmail.com>
2019-02-22 17:24:28 -0500

    collocations

--- justificationmap.py
+++ justificationmap.py
@@ -2,6 +2,8 @@ import sys
 import codecs
 import nltk
 
+from nltk.collocations import *
+
 """
 Use Natural Language Tool Kit.
 Script that can analyze text for soundness and completeness in arguments.
@@ -11,10 +13,13 @@ Call this a justification map.
 """
 file = codecs.open("/Users/syedather/Desktop/thoughts.txt", "r", "utf-8-sig")
 
+# Tokenize the file of all the words and grammar characters.
 text = file.read()
 a = nltk.word_tokenize(text)
 
 just = {} # justification map
 
-
-
+# Collocations to figure out where multiple words commonly occur together.
+bigram_measures = nltk.collocations.BigramAssocMeasures()
+trigram_measures = nltk.collocations.TrigramAssocMeasures()
+finder = BigramCollocationFinder.from_words(nltk.corpus.genesis.words(text))
