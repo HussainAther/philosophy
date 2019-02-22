523bae498117687f41d4b4a188096e4d71f2128b
Syed Hussain Ather <shussainather@gmail.com>
2019-02-22 17:51:23 -0500

    Speculate on future uses

--- justificationmap.py
+++ justificationmap.py
@@ -10,6 +10,7 @@ Script that can analyze text for soundness and completeness in arguments.
 Require user to justify claims using other claims in the text.
 Create a logical map that shows how claims are justified.
 Call this a justification map.
+Check how frequently words are used with one another.
 """
 file = codecs.open("/Users/syedather/Desktop/thoughts.txt", "r", "utf-8-sig")
 
