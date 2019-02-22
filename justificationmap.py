0492e22909914f7f31970ec384ddb68105b19d77
Syed Hussain Ather <shussainather@gmail.com>
2019-02-21 20:04:23 -0500

    ntlk

--- justificationmap.py
+++ justificationmap.py
@@ -1,6 +1,8 @@
 import sys
+import ntlk
 
 """
+Use Natural Language Tool Kit.
 Script that can analyze text for soundness and completeness in arguments.
 Require user to justify claims using other claims in the text.
 Create a logical map that shows how claims are justified.
@@ -8,4 +10,8 @@ Call this a justification map.
 """
 file = sys.argv[1]
 
-with open()
+just = {} # justification map
+
+with open(file, "r") as infile:
+    for line in infile:
+
