bd2ee2de01ca193a7433ee1e0b11c5ccb6ec0139
Syed Hussain Ather <shussainather@gmail.com>
2019-03-19 10:35:22 -0400

    def

--- epist/braininvat.py
+++ epist/braininvat.py
@@ -2,3 +2,10 @@
 """
 Knowing one isn't a brain in a vat (BIV).
 """
+
+def notbiv():
+    """
+    Logical architechture for Brain in a vat argument.
+    """
+    if biv == "?": # If I don't know I'm not a biv
+        hands = "?" # I don't know that I have hands
