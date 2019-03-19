0f59a5e2f091f84c19d426f44db9f0cf4b0f1c7d
Syed Hussain Ather <shussainather@gmail.com>
2019-03-18 22:01:18 -0400

    initial commit'

---
+++ logic/falsifiability.py
@@ -0,0 +1,4 @@
+
+"""
+We say that a theory is falsified only if we have accepted basic statements which contradict it (cf. section 11, rule 2). This condition is necessary, but not sufficient; for we have seen that non-reproducible single occurrences are of no significance to science. Thus a few stray basic statements contradicting a theory will hardly induce us to reject it as falsified. We shall take it as falsified only if we discover a reproducible effect which refutes the theory. In other words, we only accept the falsification if a low-level empirical hypothesis which describes such an effect is proposed and corroborated. This kind of hypothesis may be called a falsifying hypothesis.1 The requirement that the falsifying
+"""
