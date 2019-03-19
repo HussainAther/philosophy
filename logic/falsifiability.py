c1f495ef506165051a6f6641e60ba0a930c3f3a0
Syed Hussain Ather <shussainather@gmail.com>
2019-03-18 22:10:55 -0400

    relfreq

--- logic/falsifiability.py
+++ logic/falsifiability.py
@@ -31,4 +31,23 @@ denoted by ‘N(α)’, to be read ‘the number of α’. Now let there be anot
 
 We call β our property-class: it may be, for example, the class of all throws which show a five, or (as we shall say)
 which have the property five.
+
+The class of those elements which belong to both α and β, for example the class of throws made yesterday with this
+particular die and having the property five, is called the product-class of α and β, and is denoted by ‘α.β’, to be
+read ‘α and β’. Since α.β is a subclass of α, it can at most contain a finite number of elements (it may be empty).
+
+The number of elements in α.β is denoted by ‘N(α.β)’.
+
+Whilst we symbolize (finite) numbers of elements by N, the relative frequencies are symbolized by F′′. For example,
+‘the relative frequency of the property β within the finite reference-class α’ is written ‘αF′′(β)’, which may be read
+‘the α-frequency of β’. We can now define the relative frequency.
 """
+
+def relfreq(n, alpha, beta):
+    """
+    For some function n that returns the number of fives thrown yesterday with this die when
+    given alpha and beta, and, when given only alpha, it returns the total number of throws yesterday.
+    """
+    return n(alpha, beta) / n(alpha)
+
+
