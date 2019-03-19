8096493a8b9f9cc4d7621dac02db3f385ccdf9e4
Syed Hussain Ather <shussainather@gmail.com>
2019-03-19 10:09:04 -0400

    def

--- epist/ai.py
+++ epist/ai.py
@@ -1,3 +1,11 @@
+import numpy as np
+
 """
 Addressing epistemological problems brought upon by artifiical intelligence.
 """
+
+def concepts(knowledge):
+    """
+    This function expresses facts such as "Pat knows the combination of the 
+    safe." 
+    """
