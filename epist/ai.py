c899b5925acfab9383610ee6a9bfc5d7d7dd07f3
Syed Hussain Ather <shussainather@gmail.com>
2019-03-19 10:10:53 -0400

    safe combo

--- epist/ai.py
+++ epist/ai.py
@@ -9,3 +9,6 @@ def concepts(knowledge):
     This function expresses facts such as "Pat knows the combination of the 
     safe." 
     """
+    combination = "45-25-17" # a combination for a safe
+    if knowledge == combination:
+        print("The formalization of this knowlege is: 'The combination of the safe is %'" %f combination) 
