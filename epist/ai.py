55a65acf8a55b284536259e98b31180f7ad6a8cc
Syed Hussain Ather <shussainather@gmail.com>
2019-03-19 10:20:41 -0400

    rm def

--- epist/ai.py
+++ epist/ai.py
@@ -4,12 +4,4 @@ import numpy as np
 Addressing epistemological problems brought upon by artifiical intelligence.
 """
 
-def concepts(knowledge):
-    """
-    This function expresses facts such as "Pat knows the combination of the 
-    safe." 
-    """
-    combination = "45-25-17" # a combination for a safe
-    if knowledge == combination:
-        print("The formalization of this knowlege is: 'The combination of the safe is %'" %f combination) 
      
