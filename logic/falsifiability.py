44e4ad5e7d636894e9ac918f72f5211e8414c3d2
Syed Hussain Ather <shussainather@gmail.com>
2019-03-18 22:02:06 -0400

    comment

--- logic/falsifiability.py
+++ logic/falsifiability.py
@@ -1,4 +1,16 @@
 
 """
-We say that a theory is falsified only if we have accepted basic statements which contradict it (cf. section 11, rule 2). This condition is necessary, but not sufficient; for we have seen that non-reproducible single occurrences are of no significance to science. Thus a few stray basic statements contradicting a theory will hardly induce us to reject it as falsified. We shall take it as falsified only if we discover a reproducible effect which refutes the theory. In other words, we only accept the falsification if a low-level empirical hypothesis which describes such an effect is proposed and corroborated. This kind of hypothesis may be called a falsifying hypothesis.1 The requirement that the falsifying
+We say that a theory is falsified only if we have accepted basic statements which contradict
+it (cf. section 11, rule 2). This condition is necessary, but not sufficient; for we have seen
+that non-reproducible single occurrences are of no significance to science.
+
+Thus a few stray basic statements contradicting a theory will hardly induce us to reject it as falsified.
+We shall take it as falsified only if we discover a reproducible effect which refutes the theory. In other words,
+we only accept the falsification if a low-level empirical hypothesis which describes such an effect is
+proposed and corroborated. This kind of hypothesis may be called a falsifying hypothesis.
+
+The requirement that the falsifying hypothesis must be empirical, and so falsifiable, only means that it must
+stand in a certain logical relationship to possible basic statements; thus this requirement only concerns the
+logical form of the hypothesis. The rider that the hypothesis should be corroborated refers to tests which it
+ought to have passed—tests which confront it with accepted basic statements.
 """
