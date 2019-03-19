134fe03735085fde7310f5f7163580df7ef07391
Syed Hussain Ather <shussainather@gmail.com>
2019-03-19 10:58:46 -0400

    line len

--- aesthetics/zenofpython.py
+++ aesthetics/zenofpython.py
@@ -22,7 +22,7 @@ In the face of ambiguity, refuse the temptation to guess.
 There should be one—and preferably only one—obvious way to do it.
 Although that way may not be obvious at first unless you're Dutch.
 Now is better than never.
-Although never is often better than right now.[n 1]
+Although never is often better than right now.
 If the implementation is hard to explain, it's a bad idea.
 If the implementation is easy to explain, it may be a good idea.
 Namespaces are one honking great idea—let's do more of those!
@@ -31,7 +31,9 @@ Namespaces are one honking great idea—let's do more of those!
 with open(sys.argv[1], "r") as file:
     score = 0 
     linecount = 0
-   
-    linecount += 1
+    linelens = []
+    for line in file:
+        linelens.append(len(line))
+        linecount += 1		
     
 print(score)
