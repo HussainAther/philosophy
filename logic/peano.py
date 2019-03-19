68cebced33f262fff5d9774e8f83b201fe0a393d
Syed Hussain Ather <shussainather@gmail.com>
2019-03-18 22:39:09 -0400

    comment

--- logic/peano.py
+++ logic/peano.py
@@ -1,5 +1,8 @@
 """
 With Peano arithmetic, we can write various schemas.
+
+Its axioms – eliminating the redundancy from our original statement of the axioms – 
+are the following sentences (closed well-formed formula)
 """
 
 
