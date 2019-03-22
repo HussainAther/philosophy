96b56c17f3e90343f4e4c2aebd8f7fde1153874a
Syed Hussain Ather <shussainather@gmail.com>
2019-03-22 19:07:58 -0400

    code

--- logic/consistency.py
+++ logic/consistency.py
@@ -1,4 +1,13 @@
 
 """
-A consistency check procedure that may be used in logic solvers. 
+A consistency check procedure that may be used in logic solvers.
 """
+
+def consistent(h, phi):
+    """
+    Check graph h for consistency for some formula phi.
+    """
+    for n in h: # for each node in h
+        if phi(n) in h and phi(n) not in h:
+            return False
+    return True
