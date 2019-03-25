fd4cf92f944b89c0fa1ec4797d10fb9f13faac95
Syed Hussain Ather <shussainather@gmail.com>
2019-03-24 23:54:09 -0400

    initial commit

---
+++ epist/realism.py
@@ -0,0 +1,12 @@
+
+
+"""
+Scientific realism pertains to scientific theories and the entities they postulate. A theory postulates an entity if it is expressed in terms of the properties and behavior of the entity. For example, Mendelian genetics postulates a “gene” as a unit that controls a heritable characteristic. Eventually we discovered that genes are encoded in DNA, but for about 50 years, a gene was just a postulated entity. 
+
+We can state philosophical positions in a range of strengths, where SR1 is a weak form of scientific realism and SR4 is a strong form:
+SR1: Scientific theories are true or false to the degree that they approximate reality, but no theory is exactly true. Some postulated entities may be real, but there is no principled way to say which ones.
+SR2: As science advances, our theories become better approximations of reality. At least some postulated entities are known to be real.
+SR3: Some theories are exactly true; others are approximately true. Entities postulated by true theories, and some entities in approximate theories, are real.
+SR4: A theory is true if it describes reality correctly, and false otherwise. The entities pos- tulated by true theories are real; others are not.
+SR4 is so strong that it is probably untenable; by such a strict criterion, almost all current theories are known to be false. Most realists would accept something in the space between SR1 and SR3.
+"""
