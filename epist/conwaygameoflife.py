6a0b138232a73838956602d946f207d579c4510a
Syed Hussain Ather <shussainather@gmail.com>
2019-03-25 01:20:17 -0400

    import

--- epist/conwaygameoflife.py
+++ epist/conwaygameoflife.py
@@ -1,5 +1,11 @@
 import scipy.ndimage	
 import numpy as np
+import matplotlib.pyplot as plt
+
+"""
+In Conway's game of Life (Conway), we watch how a system of cellular
+automata (CA) evolve over time.
+"""
 
 kernel = np.array([[1,1,1],
                       [1,0,1],
@@ -8,3 +14,9 @@ kernel = np.array([[1,1,1],
 # we use convolution (from digital image processing) to
 # compute functions based on an image's pixels
 neighbors = scipy.ndimage.filters.convolve(array, kernel)
+
+def step(a, w, m):
+    """
+    Step forward in time for some array a with weights w and mode m.
+    """
+    con = scipy.ndimage.filters.convolve(a, w, m)
