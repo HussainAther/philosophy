0cb0f7bc3197b099bb98ccf3cdd0addb68b05159
Syed Hussain Ather <shussainather@gmail.com>
2019-03-25 00:43:57 -0400

    initial commit

---
+++ epist/cellularautomata.py
@@ -0,0 +1,13 @@
+"""
+Stephen Wolfram believes cellular automata (CAs) can be grouped into
+four classes based on their behavior. 
+
+Class 1 is the simplest in which CAs evolve from almost any starting
+    condition to the same uniform pattern.
+Class 2 generates a simple pattern with nested structure such that
+    the pattern contains many smaller versions of itself. Some Class 2
+    CAs can generate patterns that are intricate and beautiful, and still
+    simple.
+Class 3 has randomness.
+Class 4 are common in the natural world.
+"""
