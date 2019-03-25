712820eab75c488928f35f20ac0aa72f0fda6256
Syed Hussain Ather <shussainather@gmail.com>
2019-03-25 01:21:59 -0400

    weight

--- epist/conwaygameoflife.py
+++ epist/conwaygameoflife.py
@@ -7,9 +7,11 @@ In Conway's game of Life (Conway), we watch how a system of cellular
 automata (CA) evolve over time.
 """
 
-kernel = np.array([[1,1,1],
-                      [1,0,1],
-                      [1,1,1]])
+w = np.array([[1,1,1],[1,0,1],[1,1,1]])
+
+n = 5 # grid size
+m = "wrap" # used for numpy's convolve 
+a = np.random.random_integers(0, 1, (n, n))
 
 # we use convolution (from digital image processing) to
 # compute functions based on an image's pixels
@@ -20,3 +22,5 @@ def step(a, w, m):
     Step forward in time for some array a with weights w and mode m.
     """
     con = scipy.ndimage.filters.convolve(a, w, m)
+
+
