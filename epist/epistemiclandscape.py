d454b436228c30190810047f7ef6e9736bc7f35c
Syed Hussain Ather <shussainather@gmail.com>
2019-03-27 00:01:55 -0400

    initial commit

---
+++ epist/epistemiclandscape.py
@@ -0,0 +1,11 @@
+"""
+In researching how scientists form conclusions and how fast various groups
+of researchers can discover significan results, philosophers Weisberg and Muldoon
+created 3D "epistemic landscapes" in which the x and y axes of a graph represent
+research programs with the vertical z axis the scientific importance of the results
+attainable by the research program. They created algorithms for three processes:
+(1) scientists follow the control research strategy and ignore what other scientists
+are doing, (2) scientists follow methods of research their predecessors used, and (3)
+scientists take into account results of others such that, if a method has already been
+explored, they adopt a different one.
+"""
