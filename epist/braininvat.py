1871dc99613a62dacae1f994bbe42437b9525183
Syed Hussain Ather <shussainather@gmail.com>
2019-03-26 20:59:05 -0400

    syntax

--- epist/braininvat.py
+++ epist/braininvat.py
@@ -1,12 +1,17 @@
 
 """
-Knowing one isn't a brain in a vat (BIV).
+Knowing one isn't a brain in a vat (BIV, biv).
 Let's look at various methods of reasoning.
 """
 
-def notbiv():
+def simplenotbiv():
     """
-    Logical architechture for Brain in a vat argument.
+    Logical architechture for a simple brain in a vat argument.
     """
     if biv == "?": # If I don't know I'm not a biv
         hands = "?" # I don't know that I have hands
+
+
+"""
+
+"""
