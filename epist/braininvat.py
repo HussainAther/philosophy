b1637594715191be11790987e8f814fdbbd83d0f
Syed Hussain Ather <shussainather@gmail.com>
2019-03-26 21:07:24 -0400

    it is me

--- epist/braininvat.py
+++ epist/braininvat.py
@@ -1,3 +1,4 @@
+knowledge = ["P"] # what we know
 
 """
 Knowing one isn't a brain in a vat (BIV, biv).
@@ -6,27 +7,27 @@ Let's look at various methods of reasoning.
 
 def simplenotbiv():
     """
-    Logical architechture for a simple brain in a vat argument.
+    Logical architechture for a simple brain in a vat argument and its effect
+    on whether we have hands.
     """
-    if biv == "?": # If I don't know I'm not a biv
-        hands = "?" # I don't know that I have hands
-
+    if "biv" not in knowledge: # If I don't know I'm not a biv
+        return("I don't know that I have hands.")
+     
 
 """
 Pretty simple, but doesn't tell us much. Let's form a skeptical argument
 about how strong the biv scenario is on our access to knowledge. 
 """
 
-knowledge = ["P"] # what we know
-
 def knowP(P):
     """
     Skeptical argument for some belief or claim p on the external world. 
     If you can't be sure that you are not a brain in a vat,
     then you can't rule out the possibility all your beliefs about the external 
-    world are false. 
+    world are false.
+ 
     Descartes outlined this method of reasoning, and this line
     of reasoning was the premise for the movie "The Matrix." 
     """
-    if P i knowledge: # if we know P. Here we're using Python's 
-          
+    if P in knowledge: # if we know P. Here we're using Python's 
+         return 
