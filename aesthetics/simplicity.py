2b37056408b8d6f4ae55195338b1654809f9223d
Syed Hussain Ather <shussainather@gmail.com>
2019-03-26 20:51:54 -0400

    syntax

--- aesthetics/simplicity.py
+++ aesthetics/simplicity.py
@@ -1,9 +1,9 @@
 from inspect import signature
 
 """
-The word ‘simplicity’ is used in very many different senses. Schrödinger’s theory, for instance,
-is of great simplicity in a method- ological sense, but in another sense it might well be called
-‘complex’. We can say of a problem that its solution is not simple but difficult, or of a presentation
+The word "simplicity" is used in very many different senses. Schrödinger’s theory, for instance,
+is of great simplicity in a methodological sense, but in another sense it might well be called
+"complex." We can say of a problem that its solution is not simple but difficult, or of a presentation
 or an exposition that it is not simple but intricate.
 
 """
