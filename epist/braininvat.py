d70fd40ac8d32b8d32e73a2c88e72fc42d3183e4
Syed Hussain Ather <shussainather@gmail.com>
2019-03-26 21:00:49 -0400

    matrix

--- epist/braininvat.py
+++ epist/braininvat.py
@@ -7,11 +7,13 @@ Let's look at various methods of reasoning.
 def simplenotbiv():
     """
     Logical architechture for a simple brain in a vat argument.
+    Descartes outlined this method of reasoning, and this line
+    of reasoning was the premise for the movie "The Matrix." 
     """
     if biv == "?": # If I don't know I'm not a biv
         hands = "?" # I don't know that I have hands
 
 
 """
-
+Pretty simple, but doesn't tell us much. 
 """
