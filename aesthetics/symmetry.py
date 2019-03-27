77d9afd5cb027b5143cecb7917d70dd2692def8a
Syed Hussain Ather <shussainather@gmail.com>
2019-03-26 20:26:54 -0400

    input

--- aesthetics/symmetry.py
+++ aesthetics/symmetry.py
@@ -1,4 +1,14 @@
+import sys
+
 """
 We find symmetry as it relates to beauty and aesthetics throughout
 mathematics and science.
+
+Usage:
+
+'python symmetry.py inputfile'
 """
+
+with open(sys.argv[1]) as file: # open up some file the user inputs
+
+
