8723bb8eafae6f5f80078865df3ed53e0dd5f24e
Syed Hussain Ather <shussainather@gmail.com>
2019-03-27 00:18:04 -0400

    wright bg

--- epist/epistemiclandscape.py
+++ epist/epistemiclandscape.py
@@ -10,6 +10,12 @@ attainable by the research program. They created algorithms for three processes:
 are doing, (2) scientists follow methods of research their predecessors used, and (3)
 scientists take into account results of others such that, if a method has already been
 explored, they adopt a different one.
+
+The epistemic landscape is generally modeled after geneticist Sewall Wright's insight
+into population biology that we can represent fitness values of genotypes in terms of 
+an abstract landscape in which a particular genotypes corresponds to a point in a highly
+multidimensional landscape. The fitness value of the genotype is its height and nearby
+points are accessible via mutation.
 """
 
 
