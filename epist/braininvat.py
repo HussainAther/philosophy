01c027ce5f6c5b86fd39104e3deb9d076abd2e64
Syed Hussain Ather <shussainather@gmail.com>
2019-03-26 21:02:50 -0400

    more

--- epist/braininvat.py
+++ epist/braininvat.py
@@ -7,8 +7,6 @@ Let's look at various methods of reasoning.
 def simplenotbiv():
     """
     Logical architechture for a simple brain in a vat argument.
-    Descartes outlined this method of reasoning, and this line
-    of reasoning was the premise for the movie "The Matrix." 
     """
     if biv == "?": # If I don't know I'm not a biv
         hands = "?" # I don't know that I have hands
@@ -17,3 +15,14 @@ def simplenotbiv():
 """
 Pretty simple, but doesn't tell us much. 
 """
+
+def knowP():
+    """
+    Skeptical argument for some belief or claim p on the external world. 
+    If you can't be sure that you are not a brain in a vat,
+    then you can't rule out the possibility all your beliefs about the external 
+    world are false. 
+    Descartes outlined this method of reasoning, and this line
+    of reasoning was the premise for the movie "The Matrix." 
+    """
+    if 
