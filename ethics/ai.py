56b31faeed179bdc95a8d72dd9b0b234b73b805f
Syed Hussain Ather <shussainather@gmail.com>
2019-03-29 02:20:09 -0400

    aima

--- ethics/ai.py
+++ ethics/ai.py
@@ -1,5 +1,7 @@
 
 """
 An artificial intelligence (AI) approach to ethics can embrace many different
-ideas from philosophy.
+ideas from philosophy. In "Artificial Intelligence: A Modern Approach," (known as AIMA) 
+computer scientists Peter Norvig and Stuart Russell believe we can create goals for AI
+to act rationally. 
 """
