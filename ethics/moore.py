f5ec4bf6508671321741e14bb810ac2eace1bd70
Syed Hussain Ather <shussainather@gmail.com>
2019-03-29 03:19:59 -0400

    initial commit

---
+++ ethics/moore.py
@@ -0,0 +1,7 @@
+"""
+Philosopher James Moore believes artificial intelligence (AI) can be 
+an ethical agent that is implicit (has ethical constraints programmed 
+into it), explicit (can weigh inputs in a given ethical framework to
+choose an action), or full agency (can make ethical judgments and 
+defend its reasoning).
+"""
