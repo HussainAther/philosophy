569dbcd0aa3edbbd83bba414a3b9db20e0736157
Syed Hussain Ather <shussainather@gmail.com>
2019-03-29 03:46:07 -0400

    ppa

--- ethics/gewirth.py
+++ ethics/gewirth.py
@@ -1,4 +1,11 @@
 """
 Philosopher Alan Gewirth is known for ethical rationalism with his moral principle
-the "Principle of Generic Consistency" (PCG, pgc).
+the "Principle of Generic Consistency" (PCG, pgc). Every agent must act
+in accodrance with his/her own and all other agents' generic rights.
+For prospective purposive agents (PPA) we can derive claims.   
 """
+
+def pgc():
+    """
+    """
+    
