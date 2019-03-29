85f7d5d4381ca5a759941c997ffa30fda7bc520c
Syed Hussain Ather <shussainather@gmail.com>
2019-03-29 02:23:14 -0400

    max

--- ethics/ai.py
+++ ethics/ai.py
@@ -3,5 +3,10 @@
 An artificial intelligence (AI) approach to ethics can embrace many different
 ideas from philosophy. In "Artificial Intelligence: A Modern Approach," (known as AIMA) 
 computer scientists Peter Norvig and Stuart Russell believe we can create goals for AI
-to act rationally. 
+to act rationally. Russell asks "What is AI?" as "What is intelligence?" to identify intelligence
+closely tied with rationality. Intelligent agents can take percepts as input and act
+based upon them. We can create performance measures by calculating V the expected utility
+according to the performance measure U of the agent function f that operates on E:
+
+f_opt = max V(f, E, U)
 """
