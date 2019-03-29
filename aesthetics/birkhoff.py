47d3f11c4ab3022b93c04040e84270967301b679
Syed Hussain Ather <shussainather@gmail.com>
2019-03-29 04:46:59 -0400

    arrayorder

--- aesthetics/birkhoff.py
+++ aesthetics/birkhoff.py
@@ -2,3 +2,9 @@
 Mathematician George David Birkhoff created an aethetic measure M = O/C
 as the ratio of order to complexity.
 """
+
+def arrayordeR(a):
+    """
+    For an array a, return its order. We use a simple, rough estimation of
+    order by how sorted the array is.
+    """
