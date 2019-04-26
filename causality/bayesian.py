5bfe9a9925b355a5a81001099e0ffdd7bbb95566
Syed Hussain Ather <shussainather@gmail.com>
2019-04-26 18:00:09 -0400

    bg

--- causality/bayesian.py
+++ causality/bayesian.py
@@ -1,3 +1,12 @@
 """
-We can use a Bayesian approach to induce causal relationship.
+We can use a Bayesian approach to induce causal relationship. We look for a causal
+graph that maximizes the posterior probability
+
+p(c|D) = p(C)p(D|C) / Σp(C')p(D|C')
+
+where D is a database of observed past case data. We solve for the integral
+
+p(D|C) = integral of p(D|C,S_C)p(S_C)dS_C
+
+with probability specifications S_C that accompany C in a Bayesian net.	
 """
