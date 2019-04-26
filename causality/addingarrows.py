5bd8cf91faa50a61133c3f22e5184dcd45a5c606
Syed Hussain Ather <shussainather@gmail.com>
2019-04-26 18:22:47 -0400

    steps

--- causality/addingarrows.py
+++ causality/addingarrows.py
@@ -2,7 +2,16 @@
 There are many ways to find a net within an approximation subspace with
 maximum or close to maximum weight. The simplest is a greedy adding-arrows
 algorithm:
-1. Start with a discrete net (graph with no arrows), and, at each stage,
-    find and weigh the arrows whose addition would ensure that the net
-    remains acyclic and within the chosen subspace. 
+   Start with a discrete net (graph with no arrows), and, at each stage,
+   find and weigh the arrows whose addition would ensure that the net
+   remains acyclic and within the chosen subspace, and add one maximum 
+   weight.
+ 
+   If more than one maximum weight arrow exists, we can spawn several new 
+   nets by adding each maximum weight to the previous graph, and we can
+   constantly prune the nets under consideration by eliminating those
+   which no longer have maximum total weight.
+
+   We stop when no more arrows can be added and output the resulting Bayesian
+   nets. 
 """
