94b2e5826c5181e1efa4e10270d14382b968035e
Syed Hussain Ather <shussainather@gmail.com>
2019-05-27 20:59:49 -0400

    return str

--- logic/turingmachine.py
+++ logic/turingmachine.py
@@ -14,4 +14,13 @@ class Tape(object):
         we print on the tape. 
         """
         self.__tape = dict((enumerate(tape_string)))
-        
+       
+    def __str__(self):
+        """
+        Print out part of the tape as a string.
+        """ 
+        min_used_index = min(self.__tape.keys())
+        max_used_index = max(self.__tape.keys())
+        for i in range(min_used_index, max_used_index):
+            s += self.__tape[i]
+        return s
